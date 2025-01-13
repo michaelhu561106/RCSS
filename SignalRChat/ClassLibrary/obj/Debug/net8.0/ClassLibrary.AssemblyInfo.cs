5450f9c062f8f558f2d27f48b0be7881dc72fa51
LAPTOP-BJBNG77F\user <michaelhu561106@gmail.com>
2025-01-13 11:53:32 +0800

    新版再Push一次

--- SignalRChat/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ SignalRChat/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72da5a5252265bf2d552d703bde18a7d81a96399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24b8d978fc4154614c12e8fff74ae9861cb1732")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
